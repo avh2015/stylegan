7d5049e77ce45d0a13b195f0303451bd4d041a1b
Kamal <designerkamal@gmail.com>
2019-06-13 12:59:15 +0530

    minor pep8 fuxes

--- server.py
+++ server.py
@@ -45,12 +45,14 @@ generate_inputs = {
     'truncation': runway.number(min=0, max=1, default=0.8, step=0.01)
 }
 
-@runway.command('generate', inputs=generate_inputs, outputs={'image': runway.image})
+@runway.command('generate', inputs=generate_inputs,
+    outputs={'image': runway.image})
 def convert(model, inputs):
     z = inputs['z']
     truncation = inputs['truncation']
     latents = z.reshape((1, 512))
-    images = model.run(latents, None, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
+    images = model.run(latents, None, truncation_psi=truncation,
+        randomize_noise=False, output_transform=fmt)
     output = np.clip(images[0], 0, 255).astype(np.uint8)
     return {'image': output} """
 
